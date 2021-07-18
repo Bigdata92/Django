1c37bdfecf11fe83b08a383cdeae01f26b7adc45
Bigdata92 <wltkqdl@gmail.com>
2021-07-19 08:51:42 +0900

    django course 30 commit

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')), # account 주소로 접근
+    path('profiles/', include('profileapp.urls')),
 ]
