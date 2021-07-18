072c143ca6f07cd77ecbad98a536bdf6ad5d87dc
Bigdata92 <wltkqdl@gmail.com>
2021-07-18 20:48:45 +0900

    django course 26 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "accountapp"
 # 127.0.0.1:8000/account/hello_world 경로 대신 app_name:hello_world로 쉽게 주소 사용가능
@@ -15,6 +15,7 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'), # class형
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
     # pk 이름의 int받겠다. 몇번 유저 개체에 접근 할 것인지 적어주는것
 ]
 
